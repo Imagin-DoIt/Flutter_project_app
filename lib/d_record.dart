f44853e78c04dba6db059656abc64a2d31e6f068
김이영 <rladldud1109@naver.com>
2021-05-11 14:34:17 +0900

    수정

--- lib/d_record.dart
+++ lib/d_record.dart
@@ -88,12 +88,12 @@ class DRecordState extends State<DRecord> {
           children: <Widget>[
             UserAccountsDrawerHeader(
               currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/con.png'),
+                backgroundImage: AssetImage('assets/connie1.jpg'),
                 backgroundColor: Colors.white,
               ),
               otherAccountsPictures: <Widget>[
                 CircleAvatar(
-                  backgroundImage: AssetImage('assets/con.png'),
+                  backgroundImage: AssetImage('assets/connie2.jpg'),
                   backgroundColor: Colors.white,
                 ),
               ],
